140256af97b163e757525316c82f26660148e70e
Mike E <43850538+lachmier@users.noreply.github.com>
2020-04-30 19:40:11 +0200

    newline... and other fixes

--- src/slask.py
+++ src/slask.py
@@ -1,8 +1,12 @@
 import yaml
 import random
 
+
 def test(name):
     for i in range(10):
-        print(f'Jello {name}')
+        print(f"Jello {name}")
+        yaml.dump(name)
+        print(f"Random Number: {random.randint(1, 10)}")
 
-test('Name')
+
+test("Name")
