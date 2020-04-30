1c4febfffc4b4ae720951d9fb6d4b8c5bd5417e1
Mike E <43850538+lachmier@users.noreply.github.com>
2020-04-30 19:58:10 +0200

    New

--- src/slask.py
+++ src/slask.py
@@ -3,10 +3,11 @@ import random
 
 
 def test(name):
+    # test of stuff
     for i in range(10):
-        print(f"Jello {name}")
-        yaml.dump(name)
-        print(f"Random Number: {random.randint(1, 10)}")
+        print(f"Random Number: {random.randint(1, 10)}, Jello {name}")
+        test = yaml.dump(name)
+        print(test)
 
 
 test("Name")
